8fa503d465d910cc9cc0e9bdca3a7d2128f456ad
Umut ÇAKIR <umutcakirbm@gmail.com>
2022-06-11 21:33:31 +0300

    Setup redux toolkit basic configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -22,9 +24,28 @@ module.exports = {
   plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
     '@typescript-eslint/no-explicit-any': 1,
+    '@typescript-eslint/explicit-module-boundary-types': [
+      'error',
+      {
+        allowedNames: ['useAppDispatch'],
+      },
+    ],
     camelcase: 'error',
     quotes: ['error', 'single'],
     'no-duplicate-imports': 'error',
+    'no-console': 1,
+    'no-param-reassign': ['error'],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        alphabetize: {
+          order: 'asc',
+        },
+        'newlines-between': 'always',
+      },
+    ],
+    'import/no-named-as-default-member': 'off',
   },
   settings: { react: { version: '17.0.2' } },
-}
+};
