bf7c37fae6f203d630b4479c0c8f904b486d5104
Umut ÇAKIR <umutcakirbm@gmail.com>
2022-06-11 20:09:29 +0300

    Setup prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,28 @@ module.exports = {
     browser: true,
     amd: true,
     node: true,
+    jest: true,
   },
-  parser: "@typescript-eslint/parser",
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
   rules: {
-    "@typescript-eslint/no-explicit-any": 1,
+    '@typescript-eslint/no-explicit-any': 1,
+    camelcase: 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
   },
-};
+  settings: { react: { version: '17.0.2' } },
+}
