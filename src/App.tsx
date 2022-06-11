51076d8eb2c6477144a0656967be7b677ab2f5c1
Umut ÇAKIR <umutcakirbm@gmail.com>
2022-06-11 22:58:53 +0300

    Add Sass support, default theme and normalize css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const App: React.FC = () => <h1>Plan your trip!</h1>;
+const App: React.FC = () => {
+  // TODO: Move this to redux state and add theme switch
+  const [theme] = useState('theme--default');
+
+  return (
+    <main className={`theme ${theme}`}>
+      <h1>Plan your trip!</h1>
+    </main>
+  );
+};
 
 export default App;
